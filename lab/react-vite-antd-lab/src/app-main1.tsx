17dd32d70e61f37ded95605d06d353f3c81e8c35
Sushant Agrawal <capitalch@gmail.com>
2022-12-13 21:36:24 +0530

    init'

--- lab/react-vite-antd-lab/src/app-main1.tsx
+++ lab/react-vite-antd-lab/src/app-main1.tsx
@@ -1,6 +1,6 @@
 import { Button, ConfigProvider, DatePicker, Image, Layout, Space, Typography, theme, version, Divider } from 'antd'
 import { LaptopOutlined, LeftOutlined, MenuOutlined, MenuUnfoldOutlined, NotificationOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { blue, generate, presetPalettes, presetDarkPalettes } from '@ant-design/colors'
+import { generate, presetPalettes, presetDarkPalettes, geekblue } from '@ant-design/colors'
 import type { MenuProps } from 'antd';
 import { Breadcrumb, Menu, } from 'antd';
 import React, { useState } from 'react';
@@ -14,6 +14,7 @@ import {
 const { Content, Footer, Header, Sider } = Layout
 import { Button as MuiButton, Box } from '@mui/material'
 import { Container, display, SxProps } from '@mui/system';
+import { Calculate } from '@mui/icons-material';
 // import './app-main1.scss'
 
 function AppMain1() {
@@ -31,7 +32,7 @@ function AppMain1() {
         // width={200}
         >
             {/* <Space size='large' direction='vertical' > */}
-                {/* <Space size='large'>
+            {/* <Space size='large'>
                     <Image
                         style={{ marginLeft: '10px', marginTop: '5px' }}
                         width={140}
@@ -43,26 +44,31 @@ function AppMain1() {
                         style={{ marginTop: '1rem', marginRight: '.5rem' }}
                     />
                 </Space> */}
-                {/* <Divider /> */}
-                <Menu mode='vertical' defaultSelectedKeys={['4']} theme='light' style={{ height: '100vh', width: '200px' }}
-                    items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-                        (icon, index) => ({
-                            key: String(index + 1),
-                            icon: React.createElement(icon),
-                            label: `nav ${index + 1}`,
-                        }),
-                    )} />
+            {/* <Divider /> */}
+            <Menu mode='vertical' defaultSelectedKeys={['4']} theme='light' style={{ height: '100vh', width: '200px' }}
+                items={[UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+                    (icon, index) => ({
+                        key: String(index + 1),
+                        icon: React.createElement(icon),
+                        label: `nav ${index + 1}`,
+                    }),
+                )} />
             {/* </Space> */}
         </Sider>
         <Layout>
-            <Header  style={{ width: '100vw'}}>Header</Header>
-            <Content style={{backgroundColor:'red', height:'200px'}}>Content</Content>
+            {/* <Header  style={{ width: '100vw'}}>Header</Header> */}
+            <div style={{ width: 'calc(100vw - 200px)', backgroundColor: geekblue.primary, height: '38px', color: 'white' }}>
+                <Space >
+                    <Button type='ghost' style={{ color: 'white', verticalAlign: 'center' }}>Test</Button>
+                </Space>
+            </div>
+            <Content style={{ backgroundColor: 'red', height: '200px' }}>Content</Content>
         </Layout>
         {/* <Content style={{ width: 'calc(100vw - 200px)' , padding: '10px' }}> */}
-            {/* <Container  sx={{ backgroundColor: 'red', height: '56px' }}>
+        {/* <Container  sx={{ backgroundColor: 'red', height: '56px' }}>
                 <Button size='small'>Test</Button>
             </Container> */}
-            {/* <Space direction='vertical'>
+        {/* <Space direction='vertical'>
                 <Space direction='vertical' size='large'>
                     <Box sx={{ display: 'flex', width: 'auto', backgroundColor: 'red' }}>
                         <Button size='small'>Test</Button>
