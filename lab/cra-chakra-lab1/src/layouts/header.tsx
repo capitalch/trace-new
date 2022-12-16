e9151d4e65eb66539896b804e3dcfb40f0ffa0ca
capitalch <capitalch@gmail.com>
2022-12-17 00:38:53 +0530

    init

--- lab/cra-chakra-lab1/src/layouts/header.tsx
+++ lab/cra-chakra-lab1/src/layouts/header.tsx
@@ -1,11 +1,23 @@
 import { useEffect } from 'react'
-import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, HStack, Input, Slide, useDisclosure } from "@chakra-ui/react"
+import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Flex, Hide, HStack, Input, Show, Slide, useBreakpointValue, useDisclosure, useMediaQuery } from "@chakra-ui/react"
 
 function LayoutMain() {
     const { isOpen, onClose, onOpen, onToggle } = useDisclosure()
+    const { isOpen: io, onClose: oc, onOpen: oo, onToggle: ot }: any = useDisclosure()
+    // const toOpen = useBreakpointValue({
+    //     md: true,
+    //     xl: false,
+    //     '2xl': false,
+    //     sm: 'true',
+    // })
+    const [isLargerThan1536] = useMediaQuery("(min-width: 1536px)", { ssr: false, })
     useEffect(() => {
-        onOpen()
-    }, [])
+        if (isLargerThan1536) {
+            onOpen()
+        } else {
+            onClose()
+        }
+    },)
     return (
         <Box>
             <Header isOpen={isOpen} />
@@ -29,16 +41,19 @@ export { Header }
 
 function LeftDrawer({ isOpen, onClose, onOpen }: any) {
     return (
+        // <Hide below='2xl'>
         <Slide direction='left' in={isOpen} style={{ zIndex: 10 }} >
             <Box
                 bg='teal.300'
                 mt='39px'
-                w={[0,0,0,0,0,200]}
+                w={200}
+                // w={[0, 0, 0, 0, 0, 200]}
                 h='100%'
                 shadow='xs'
-                display='none'
+            // display='none'
             >ABCD</Box>
         </Slide>
+        // </Hide>
         // </>
         // <Drawer isOpen={isOpen}
         //     size='xs'
@@ -70,10 +85,10 @@ export { LeftDrawer }
 
 function Content({ isOpen }: any) {
     return (
-        <Box  
-        w='calc(100vw - 200px)' 
-        ml='200px'
-        bg='gray.400' h="calc(100vh - 39px)" >AAA</Box>
+        <Box
+            w={isOpen? 'calc(100vw - 200px)': '100vw'}
+            ml= {isOpen? '200px': 0}
+            bg='gray.400' h="calc(100vh - 39px)" >AAA</Box>
         // <Flex minW='100%' minH='100%' bg='gray.100' ml={200}>
         //     <Button colorScheme='blue'>Content button</Button>
         // </Flex>
