4d352016772021172c0f74bb8a294da11c465598
capitalch <capitalch@gmail.com>
2022-12-20 14:59:22 +0530

    init

--- lab/cra-chakra-lab2/craco.config.js
+++ lab/cra-chakra-lab2/craco.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@src': path.resolve(__dirname, 'src'),
-      "@features1/*": path.resolve(__dirname,'src')
+      // '@src': path.resolve(__dirname, 'src'),
+      "@features1": path.resolve(__dirname,'src')
     },
   },
 };
