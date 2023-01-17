5c18b1b85ec244d6455ba62d30933a819d0787c8
capitalch <capitalch@gmail.com>
2023-01-17 13:21:51 +0530

    init

--- lab/cra-chakra-ag-grid/src/components/comp2-memo.tsx
+++ lab/cra-chakra-ag-grid/src/components/comp2-memo.tsx
@@ -1,15 +1,15 @@
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, HStack, VStack } from "@chakra-ui/react";
 import { Button } from "antd";
-import { useMemo, useState } from "react";
+import {useCallback, useMemo, useState } from "react";
 
 function Comp2Memo() {
   const [count, setCount] = useState(0);
-  const [toDoCount, setToDoCount] = useState(0)
-  const myVal = useMemo(() => myFunc(), [count]);
+  const [toDoCount, setToDoCount] = useState(0);
+  const myVal:any = useCallback(() => myFunc, [count]);
   return (
     <Box>
       <VStack>
-        <span>Value is: {myVal}</span>
+        <HStack>Value is: {myVal()}</HStack>
         <span>{toDoCount}</span>
         <Button
           onClick={() => {
@@ -18,19 +18,33 @@ function Comp2Memo() {
         >
           Click
         </Button>
-        <Button onClick={()=>{
-            setToDoCount(toDoCount + 1)
-        }} >To do</Button>
+        <Button
+          onClick={() => {
+            setToDoCount(toDoCount + 1);
+          }}
+        >
+          To do
+        </Button>
       </VStack>
     </Box>
   );
 
+
   function myFunc() {
+    // newStyleDelay()
+    setToDoCount(toDoCount + 1);
     let ret = 0;
     for (let i = 0; i < 1000; i++) {
       ret++;
     }
-    return ret;
+    // return ret;
   }
 }
 export { Comp2Memo };
+
+// async function newStyleDelay() {
+//   await setTimeout(() => {
+//     setToDoCount(toDoCount + 1)
+//   }, 10000);
+//   console.log("It will be printed 3-rd with delay");
+// }
