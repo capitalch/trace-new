43c3aa43dfd1a7a04446392ebe8225147add874c
capitalch <capitalch@gmail.com>
2023-02-08 11:06:02 +0530

    init

--- dev/trace-api-server/trace-server/app/db/helpers/db_helper_psycopg2.py
+++ dev/trace-api-server/trace-server/app/db/helpers/db_helper_psycopg2.py
@@ -153,31 +153,3 @@ def process_deleted_ids(sqlObject, acur: Any):
     ret = ret.rstrip(',') + ')'
     sql = f'''delete from "{tableName}" where id in{ret}'''
     acur.execute(sql)
-
-# def exec_sql(dbName: str = Config.DB_AUTH_DATABASE, db_params: dict[str, str] = dbParams, schema: str = 'public', sql: str = None, sqlArgs: dict[str, str] = {}):
-#     dbName = Config.DB_AUTH_DATABASE if dbName is None else dbName
-#     db_params = dbParams if db_params is None else db_params
-#     schema = 'public' if schema is None else schema
-#     db_params.update({'dbname': dbName})
-#     # creates connInfo from dict object
-#     connInfo = make_conninfo('', **dbParams)
-
-#     records = None
-#     try:
-#         connPool = get_connection_pool(connInfo, dbName)
-#         conn = connPool.getconn()
-#         # conn = psycopg2.connect(connInfo)
-#         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-#         cur.execute(f'set search_path to {schema}')
-#         cur.execute(sql, sqlArgs)
-#         if (cur.rowcount > 0):
-#             records = cur.fetchall()
-
-#         cur.close()
-#         conn.close()
-       
-#     except Exception as e:
-#         raise AppHttpException(
-#             detail=Messages.err_invalid_access_token, status_code=status.HTTP_401_UNAUTHORIZED
-#         )
-#     return (records)
