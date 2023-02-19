f48d1c437b5e92538709aa1ffae673513a23cdec
Sushant Agrawal <capitalch@gmail.com>
2023-02-19 20:58:35 +0530

    init

--- dev/trace-client/src/features/components/app-loader.tsx
+++ dev/trace-client/src/features/components/app-loader.tsx
@@ -1,10 +1,10 @@
-import { appStore, Center, Flex, Modal, ModalBody, ModalContent, ModalOverlay, Spinner } from '@src/features'
+import { appStore, Modal, ModalBody, ModalOverlay, Spinner } from '@src/features'
 function AppLoader() {
     const isOpen = appStore.appLoader.isOpen.value
     return (
-        <Modal  isOpen={isOpen} onClose={handleOnClose} closeOnOverlayClick={false} >
+        <Modal isOpen={isOpen} onClose={handleOnClose} closeOnOverlayClick={false} >
             <ModalOverlay />
-            <ModalBody  zIndex={100000}style={{ position: 'absolute', left: '50%', bottom: '40%' }} >
+            <ModalBody zIndex={100000} style={{ position: 'absolute', left: '50%', bottom: '40%' }} >
                 <Spinner size='xl' colorScheme='blue' />
             </ModalBody>
         </Modal>
