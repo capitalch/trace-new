629d30cfcffedc9193ee68245dccad4b2f29712e
Sushant Agrawal <capitalch@gmail.com>
2023-07-20 15:10:44 +0530

    init

--- lab/react-hookstate/src/immer-components/immer2.tsx
+++ lab/react-hookstate/src/immer-components/immer2.tsx
@@ -3,17 +3,18 @@ import { produce } from 'immer'
 import { useState } from "react"
 
 function Immer2() {
-const [, setRefresh] = useState({})
+    const [, setRefresh] = useState({})
     return (<Box>
         <Box m={20}>{getItems()}</Box>
         <Button m={10} onClick={handleAdd}>Add</Button>
+        <Button m={10} onClick={handleCreateHash}>Create</Button>
     </Box>)
 
     function handleAdd() {
         CompStore.rows = produce(CompStore.rows, (draft: any[]) => {
             draft.push('abc')
         })
-        const rs:any[] = CompStore.rows
+        const rs: any[] = CompStore.rows
         rs.push('abc')
 
         setRefresh({})
@@ -24,6 +25,21 @@ const [, setRefresh] = useState({})
             return (<div key={index}>{row}</div>)
         })
         return (items)
+    }
+
+    function handleCreateHash() {
+        const obj = {address1: '123', address2: '234', pin:'123'}
+        const str = Object.values(obj).join('')
+        const ret = getHash(str)
+        alert(ret)
+    }
+
+    function getHash(s: string) {
+        var h = 0, l = s.length, i = 0;
+        if (l > 0)
+            while (i < l)
+                h = (h << 5) - h + s.charCodeAt(i++) | 0;
+        return h;
     }
 }
 
