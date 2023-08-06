7c92e90be71a7ab80c43cc3080259576f430a6b5
Sushant Agrawal <capitalch@gmail.com>
2023-08-06 20:33:04 +0530

    init

--- lab/react-hookstate/src/components/main.tsx
+++ lab/react-hookstate/src/components/main.tsx
@@ -3,20 +3,21 @@ import { appStore, AppStoreType } from '@src/features'
 import { useHookstate, State } from '@hookstate/core'
 // import { Comp11A, Comp11B } from '@src/components'
 import { selectComponent, AppContent } from "@src/components"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faFilePdf } from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
     const store: State<AppStoreType> = useHookstate<AppStoreType>(appStore)
     const compName = store.layouts.compName.value
     const SelectedComponent: any = selectComponent[compName]
 
-    const MyComp = store.layouts.get({noproxy:true}).comp
+    const MyComp = store.layouts.get({ noproxy: true }).comp
     const CompA = selectComponent['comp11A']
     const CompB = selectComponent['comp11B']
     // const myFunction = store.myFunction.get({noproxy: true})
     const my = appStore.get({ noproxy: true }).myFunction()
     const comp = appStore.layouts.get({ noproxy: true }).comp
-    
+
     return (<Box m={15}>
         <SelectedComponent />
         <MyComp />
@@ -25,8 +26,11 @@ function Main() {
         <Text>Main</Text>
         <CompA />
         <CompB />
+        <FontAwesomeIcon icon={faFilePdf} size = 'xl' color="red" />
+        {/* <FontAwesomeIcon icon={faCoffee} size="lg" color="red" /> */}
         {/* <Comp11A />
         <Comp11B /> */}
+
     </Box>)
 }
 export { Main }
