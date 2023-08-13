c21959161fd16f23d9b69ce2ac3b4b2b2090d5aa
Sushant Agrawal <capitalch@gmail.com>
2023-08-13 21:15:37 +0530

    init'

--- lab/react-use-reducer-redux/src/features/multi-counter/multi-counter-slice.ts
+++ lab/react-use-reducer-redux/src/features/multi-counter/multi-counter-slice.ts
@@ -1,7 +1,7 @@
-import { Slice, createSlice } from "@reduxjs/toolkit";
+import { Slice, createSlice } from '@reduxjs/toolkit'
 
 const multiCounterSlice: Slice = createSlice({
-  name: "multiCounter",
+  name: 'multiCounter',
   initialState: {
     count: 0,
     count1: 0,
@@ -11,48 +11,65 @@ const multiCounterSlice: Slice = createSlice({
       level1: {
         level2: {
           level3: {
-            count: 0,
-          },
-        },
-      },
-    },
+            countA: 0,
+            countB: 0
+          }
+        }
+      }
+    }
   },
   reducers: {
     increment1: (state: any, obj: any) => {
-      state.count = state.count + obj.payload;
-      state.count1 = state.count1 + obj.payload;
+      state.count = state.count + obj.payload
+      state.count1 = state.count1 + obj.payload
     },
     increment2: (state: any, obj: any) => {
-      state.count = state.count + obj.payload;
-      state.count2 = state.count2 + obj.payload;
+      state.count = state.count + obj.payload
+      state.count2 = state.count2 + obj.payload
     },
     increment3: (state: any) => {
-      state.count = state.count + 3;
-      state.count3 = state.count3 + 3;
+      state.count = state.count + 3
+      state.count3 = state.count3 + 3
     },
-    incrementNested: (state: any) => {
-      state.nested.level1.level2.level3.count += 1;
+    incrementNestedA: (state: any) => {
+      state.nested.level1.level2.level3.countA += 1
+    },
+    incrementNestedB: (state: any) => {
+      state.nested.level1.level2.level3.countB += 1
     },
 
     decrement1: (state: any) => {
-      state.count = state.count - 1;
-      state.count1 = state.count1 - 1;
+      state.count = state.count - 1
+      state.count1 = state.count1 - 1
     },
     decrement2: (state: any) => {
-      state.count = state.count - 2;
-      state.count2 = state.count - 2;
+      state.count = state.count - 2
+      state.count2 = state.count - 2
     },
     decrement3: (state: any) => {
-      state.count = state.count - 3;
-      state.count3 = state.count3 - 3;
+      state.count = state.count - 3
+      state.count3 = state.count3 - 3
     },
     decrementNested: (state: any) => {
-      state.nested.level1.level2.level3.count -= 1;
-    },
-  },
-});
+      state.nested.level1.level2.level3.countA -= 1
+    }
+  }
+})
 
-const { reducer } = multiCounterSlice;
-const { increment1, increment2, increment3, incrementNested } = multiCounterSlice.actions;
+const { reducer } = multiCounterSlice
+const {
+  increment1,
+  increment2,
+  increment3,
+  incrementNestedA,
+  incrementNestedB
+} = multiCounterSlice.actions
 
-export { reducer as multiCounterReducer, increment1, increment2, increment3, incrementNested };
+export {
+  reducer as multiCounterReducer,
+  increment1,
+  increment2,
+  increment3,
+  incrementNestedA,
+  incrementNestedB
+}
