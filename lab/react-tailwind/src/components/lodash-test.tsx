d26b40f7b0e649b51bd26c08a4781581fc7e2796
Sushant Agrawal <capitalch@gmail.com>
2023-09-23 17:40:10 +0530

    init

---
+++ lab/react-tailwind/src/components/lodash-test.tsx
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+
+function LodashTest(){
+
+    return(<div m-4>
+        <button onClick={handleInter}>Intersection</button>
+    </div>)
+
+    function handleInter(){
+        const arr1 = []
+        const arr2 = []
+        const interArray = 
+    }
+}
+export {LodashTest}
