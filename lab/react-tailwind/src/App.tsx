f84b5fc74715cf70038558e58a3ebd712b705a9e
Sushant Agrawal <capitalch@gmail.com>
2023-09-24 08:13:11 +0530

    init

--- lab/react-tailwind/src/App.tsx
+++ lab/react-tailwind/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { PictureUpload } from './components/picture-upload';
+import { LodashTest } from './components/lodash-test';
 
 function App() {
   return (
     <div className='m-4'>
       <PictureUpload />
+      <LodashTest />
     </div>
   );
 }
