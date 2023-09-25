9595ab2077381118552bc8d59c46d46bec1717b0
capitalch <capitalch@gmail.com>
2023-09-25 13:36:05 +0530

    init'

---
+++ lab/react-bootstrap/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Comp1 } from './components/comp1';
+
+function App() {
+  return (
+    <div>
+      <Comp1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab/react-bootstrap/src/components/comp1.tsx
@@ -0,0 +1,14 @@
+function Comp1() {
+    return (<div className="d-flex m-4 justify-content-between ">
+        <div className="d-flex align-items-center ">
+            <button className="btn-link btn btn-sm btn-outline-light btn-toolbar  ">My button</button>
+            <span>This is some text</span>
+        </div>
+        <div className="d-flex align-items-center ">
+            <div>No of items</div>
+            <button className="ml-2">Another button</button>
+        </div>
+
+    </div>)
+}
+export { Comp1 }
