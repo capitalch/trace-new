0d6abcdaea20342614e81b37b3684982db81a2fb
Sushant Agrawal <capitalch@gmail.com>
2023-10-02 21:12:20 +0530

    init

---
+++ lab/react-tailwind/src/components/tailwind-use-various-events/tailwind-states.tsx
@@ -0,0 +1,11 @@
+function TailWindStates() {
+    return (<div className="m-4">
+        <button className="
+    bg-zinc-300 rounded-sm px-2 py-0.5 hover:bg-zinc-400 hover:border-2 hover:border-red-400 hover:focus:enabled:text-fuchsia-600
+    ">Various events</button>
+        <div>
+            <button className="md:bg-red-500 md:w-0">Responsive width</button>
+        </div>
+    </div>)
+}
+export { TailWindStates }
