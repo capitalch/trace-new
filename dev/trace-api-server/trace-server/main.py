2197a714de6ffe8d48549acb07199c482b1ad481
capitalch <capitalch@gmail.com>
2023-11-27 13:43:14 +0530

    init

--- dev/trace-api-server/trace-server/main.py
+++ dev/trace-api-server/trace-server/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   'http://127.0.0.1:3000', "http://localhost:3001"],
+                   'http://127.0.0.1:3000', "http://localhost:3001",'*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
